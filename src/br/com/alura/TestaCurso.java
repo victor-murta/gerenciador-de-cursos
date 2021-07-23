27c968160158241f9097f1498c4c7c26f34d53d4
victor-murta <vmurtag16@gmail.com>
2021-07-23 17:32:10 -0300

    Java

--- src/br/com/alura/TestaCurso.java
+++ src/br/com/alura/TestaCurso.java
@@ -1,16 +1,21 @@
 package br.com.alura;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class TestaCurso {
     public static void main(String[] args) {
         Curso javaColecoes = new Curso("Dominando as coleções", "Victor");
 
-        List<Aula> aulas = javaColecoes.getAulas();
+        List<Aula> aulasImutaveis = javaColecoes.getAulas();
         javaColecoes.adiciona(new Aula("Array", 22));
+        javaColecoes.adiciona(new Aula("Criando aula", 25));
 
-        System.out.println(javaColecoes);
+        List<Aula> aulas = new ArrayList<>(aulasImutaveis);
+
+
+        System.out.println(javaColecoes.getAulas());
+        System.out.println(javaColecoes.getTempoTotal());
+
     }
-
-
 }
